9cab616f4a60901d6cad20cedad5346165d258a3
Michel de Groot <michel.de.groot@cadmatic.com>
2019-12-18 15:23:56 +0100

    skip if()

--- convert.py
+++ convert.py
@@ -280,7 +280,7 @@ class FileParser(object):
         function_args_count: set to the expected number of arguments
         """
         func_name_match = re.search(FileParser.function_name_re, line)
-        if func_name_match:
+        if func_name_match and line.strip()[:2] != 'if':
             self.accumulated_lines.append(line)
 
             # Grab expected number of arguments
